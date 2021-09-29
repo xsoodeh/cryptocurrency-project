6c9c43b690e104d5153ed2983935c9d17a04b642
fateme <f.sahebi.fs@gmail.com>
2021-09-29 04:20:20 +0330

    4rd commit for update pic and css files

--- js/menu-java-script.js
+++ js/menu-java-script.js
@@ -1,11 +1,19 @@
 function openFunction() {
     let element = document.getElementById("menu-mob");
+    let mainElement = document.getElementById("main-elements");
     element.classList.add("menu-mob-show");
     element.classList.remove("menu-mob-close");
-
+    mainElement.classList.add("move-menu");
+    mainElement.classList.remove("move-back-menu");
 }
 function closeFunction() {
     let element = document.getElementById("menu-mob");
+    let mainElement = document.getElementById("main-elements");
     element.classList.remove("menu-mob-show");
     element.classList.add("menu-mob-close");
+    mainElement.classList.remove("move-menu");
+    mainElement.classList.add("move-back-menu");
+
+
+
 }
